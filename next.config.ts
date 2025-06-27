070b13a9eaa98ab66a16a5d20b5cb2a08ea8eb54
Najil Parakkal <najilparakkal@gmail.com>
2025-06-27 11:42:02 +0530

    new code

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/Shafi_ParambilE" : "",
-  assetPrefix: isProd ? "/Shafi_Parambil/" : "",
   images: {
-    domains: ["img.youtube.com", "res.cloudinary.com", "fonts.googleapis.com"],
+    domains: ["img.youtube.com",'res.cloudinary.com','fonts.googleapis.com'],
   },
 };
 
