c6cd5de5ca721e8d6d93b7b7e2b6f3e3c7ec2b1d
Najil Parakkal <najilparakkal@gmail.com>
2025-06-27 11:24:45 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["MyFont", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
