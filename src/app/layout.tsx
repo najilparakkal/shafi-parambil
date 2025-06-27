037f72952ed10d69833a6d42f9c75f6dea4b8202
Najil Parakkal <najilparakkal@gmail.com>
2025-06-27 10:51:49 +0530

    latest push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alumni+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
+
         {children}
       </body>
     </html>
