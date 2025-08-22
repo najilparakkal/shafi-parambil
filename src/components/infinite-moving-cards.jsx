b3cd0b07c436a8a7c2c39a470c4e4729a1662e6b
Najil Parakkal <najilparakkal@gmail.com>
2025-08-22 10:42:25 +0530

    dk

--- src/components/infinite-moving-cards.jsx
+++ src/components/infinite-moving-cards.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "../lib/util";
+import { cn } from "../../lib/util";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
